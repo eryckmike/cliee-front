9d59d5aecc11fa93349c686fa0c99bdbd180c63b
Eryck Mike <eryckmike@MacBook-Air-de-Eryck.local>
2025-05-07 09:57:43 -0300

    .

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ document.querySelectorAll('[data-page]').forEach(button => {
       // Alterna visibilidade de páginas
       document.querySelectorAll('.page').forEach(page => page.classList.remove('active'));
       document.getElementById(pageId).classList.add('active');
+      // Atualiza o estado do menu inferior
+document.querySelectorAll('.bottom-nav i').forEach(icon => icon.classList.remove('ativo'));
+const currentIcon = document.querySelector(`.bottom-nav i[data-page="${pageId}"]`);
+if (currentIcon) currentIcon.classList.add('ativo');
+
   
       // Troca a classe no <body> para controlar exibição de seções
       document.body.classList.toggle('perfil-ativo', pageId === 'perfil');
